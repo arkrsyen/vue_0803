c07e0ff181252cbc2b74818149ab3ccd6672752c
Biau Yen <arkrs0ch@gmail.com>
2019-09-07 01:10:34 +0800

    apipage

--- src/views/ApiTemp.vue
+++ src/views/ApiTemp.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="page api_temp">text!!</div>
+    <div class="page api_temp">
+        <div class="obj_menuBtn">
+            <ul class="leftMenu">
+                <li class="obj_Apibtn1"></li>
+                <li class="obj_Apibtn2"></li>
+                <li class="obj_Apibtn3"></li>
+                <li class="obj_Apibtn4"></li>
+            </ul>
+        </div>
+        <div class="apiBox">請輸入資料</div>
+    </div>
 </template>
 
 <script>
